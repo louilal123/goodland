44187a7faa77aeade656dfd5f4d832fb379532c4
louie <rubinlouie41@gmail.com>
2024-06-30 16:11:55 +0800

    changes commited

--- admin/classes/logout.php
+++ admin/classes/logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset(); 
 session_destroy(); 
 
-$_SESSION['status1'] = "Logout Successulyy!";
+$_SESSION['status1'] = "Logout Success!";
 $_SESSION['status_icon1'] = "success";
 header("Location: ../index.php"); 
 exit();
