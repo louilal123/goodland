4d3fa5593e833f0a260e1b07f3b1b2296fd156ca
louie <rubinlouie41@gmail.com>
2024-07-04 22:03:31 +0800

    .

--- admin/classes/connection.php
+++ admin/classes/connection.php
@@ -1,10 +1,11 @@
-
 <?php
 
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', ''");
+define('DB_PASSWORD', '');
 define('DB_NAME', 'goodland_db');
 define('DB_CHARSET', 'utf8mb4');
 
 ?>
+
+
