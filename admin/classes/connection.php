efbbbe84e0da1d8e5c4c33794eaf4d751a5668a2
louie <rubinlouie41@gmail.com>
2024-07-18 17:14:36 +0800

    commit

--- admin/classes/connection.php
+++ admin/classes/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'u510162695_goodland_db');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '1Goodland_db');
 define('DB_NAME', 'u510162695_goodland_db');
 define('DB_CHARSET', 'utf8mb4');
