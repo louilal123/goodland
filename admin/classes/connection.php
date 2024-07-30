1da114c8a0b155c88adb4ab2f6b05508be07c1b8
louie <rubinlouie41@gmail.com>
2024-07-30 15:33:08 +0800

    cm

--- admin/classes/connection.php
+++ admin/classes/connection.php
@@ -1,6 +1,8 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
+// define('DB_HOST', '127.0.0.1');
+
+define('DB_HOST', 'mysql.hostinger.com');
 define('DB_USERNAME', 'u510162695_goodland_db');
 define('DB_PASSWORD', '1Goodland_db');
 define('DB_NAME', 'u510162695_goodland_db');
