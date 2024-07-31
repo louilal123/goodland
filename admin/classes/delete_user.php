e817728fcffdc74386587b286c6e1e5f90b63cc3
louie <rubinlouie41@gmail.com>
2024-08-01 01:54:03 +0800

    commit

--- admin/classes/delete_user.php
+++ admin/classes/delete_user.php
@@ -1,11 +1,12 @@
 <?php
+
 session_start();
 
-if (isset($_POST['id'])) {
-    $user_id = intval($_POST['id']); 
+if (isset($_POST['user_id'])) {
+    $user_id = intval($_POST['user_id']); 
     require_once "Main_class.php"; 
     $conn = new Main_class();
-    $conn->delete_user($user_id); // Use $user_id here
+    $conn->delete_user($user_id);
     $_SESSION['status'] = "User deleted successfully."; 
     $_SESSION['status_icon'] = "success";
 } else {
