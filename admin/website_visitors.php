0d9b8db1df297450d789a930dbd8455b6d5577ba
louie <rubinlouie41@gmail.com>
2024-11-09 09:55:48 +0800

    c

--- admin/website_visitors.php
+++ admin/website_visitors.php
@@ -17,7 +17,7 @@
                             <div class="col-md-12">
                             <div class="card">
                                 <div class="card-body">
-                                <div class="card-header d-flex ">
+                                <div class="d-flex ">
                                   <h3 class="fw-bold">Website Visitors</h3>
                                       <button  class="btn btn-success ms-auto btn-rounded me-1" onclick="location.reload(); return false;">
                                           <i class="fas fa-refresh"></i> Refresh
@@ -27,7 +27,7 @@
                                       </button>
 
                                 </div>
-                                    <table id="myTable" class=" table table-bordered table-hover table-stripe w-100">
+                                    <table id="myTable" class="table-responsive table table-bordered table-hover table-stripe w-100">
                                         <thead class="table-secondary fw-bold">
                                             <tr>
                                                 <th>Visitor_id</th>
