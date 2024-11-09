7d045fe025c7ee3c0d64cff61a4f69ccab1aea63
louie <rubinlouie41@gmail.com>
2024-11-09 10:11:55 +0800

    c

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -17,7 +17,7 @@
     <!-- <link rel="stylesheet" href="../mdbfolder/mdb.min.css" /> -->
 
     
-<link rel="stylesheet" href="dist/custom.css">
+<!-- <link rel="stylesheet" href="dist/custom.css"> -->
     <!-- font-aswesome  -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
