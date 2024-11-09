759203b0efeebce84888aa0b3c68556ecb2aeff6
louie <rubinlouie41@gmail.com>
2024-11-09 23:59:52 +0800

    c

--- admin/all_files.php
+++ admin/all_files.php
@@ -68,7 +68,7 @@ if (isset($_GET['viewPdf']) && isset($_GET['file_path'])) {
                                                     <td><?php echo htmlspecialchars($file['id']); ?></td>
                                                     <td><?php echo htmlspecialchars($file['title']); ?></td>
                                                     <td><img src="<?php echo ($file['cover_path']); ?>" style="width: 60px; height: 35px;"></td>
-                                                    <td><a href="<?php echo ($file['file_path']); ?>" class="fw-bold text-primary">View File</a></td>
+                                                    <td><a href="uploads/<?php echo ($file['file_path']); ?>" class="fw-bold text-primary">View File</a></td>
                                                     <td><?php if ($file['status'] == 'published'): ?>
                                 <span class="badge bg-success">Published</span>
                             <?php else: ?>
