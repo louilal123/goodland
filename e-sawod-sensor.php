826a85982156fcc333bd51d23659f01fccb7943c
louie <rubinlouie41@gmail.com>
2024-11-11 11:55:45 +0800

    c

--- e-sawod-sensor.php
+++ e-sawod-sensor.php
@@ -1,26 +1,3 @@
-<?php
-
-include('classes/connection.php');
-
-// SQL query to fetch the latest data from sensor_data table
-$sql = "SELECT kit_name, level_cm, humidity, temperature, timestamp FROM sensor_data ORDER BY timestamp DESC ";
-$result = $conn->query($sql);
-
-// Check if there are results
-if ($result->num_rows > 0) {
-    // Output data of each row
-    $data = [];
-    while($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-    echo json_encode($data); // Send data as JSON
-} else {
-    echo json_encode([]); // No data found
-}
-
-// Close the connection
-$conn->close();
-?>
 <!DOCTYPE html>
 <html lang="en">
 
