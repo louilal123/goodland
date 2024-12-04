5c6a6714ee1d5e4ef6ec9e38e583070798aa49c2
louie <rubinlouie41@gmail.com>
2024-12-04 15:12:36 +0800

    c

--- admin/forgot_password.php
+++ admin/forgot_password.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$allowed_ips = ['124.217.17.43', '211111', '11223'];
+$allowed_ips = ['124.217.17.43', '103.161.61.66', '11223'];
 
 $visitor_ip = $_SERVER['REMOTE_ADDR'];
 
